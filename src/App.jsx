be4e38d80893ee1b163a2c5b4294393b29ff510a
GasKhan <mnagyi@inbox.ru>
2022-07-07 17:06:52 +0300

    Add some responsibilty

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
 import Time from './components/Time';
 import Laps from './components/Laps';
